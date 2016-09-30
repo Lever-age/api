6be895ef0a194b38d9892c5bc0083dddd4b725e8
James Lott <jlott@linode.com>
2016-09-30 15:48:30 -0400

    Make eslint happy

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+'use strict';
 
 /* External modules */
 
-var fs = require('fs')
+var fs = require('fs');
 var express = require('express');
 var winston = require('winston');
 
@@ -11,13 +12,13 @@ var CampaignInfoStorage = require('./lib/storage/sqlite/campaign-info-storage');
 
 /* Controllers */
 
-var campaigninfo_by_id = require('./controllers/campaigns').campaigninfo_by_id;
+var campaigninfoById = require('./controllers/campaigns').campaigninfo_by_id;
 
 /* App variables */
 
 var app = express();
 var config = JSON.parse(fs.readFileSync('config.json'));
-var extern = new Object();
+var extern = { logger: winston };
 
 extern.logger = winston;
 
@@ -25,11 +26,11 @@ extern.logger = winston;
 
 app.get('/campaigns/:id/info', function (req, res) {
   extern.backend = new CampaignInfoStorage(config.storage);
-  campaigninfo_by_id(extern, req, res);
+  campaigninfoById(extern, req, res);
 });
 
 /* Initialize */
 
 app.listen(config.bind.port, config.bind.address, function () {
-  extern.logger.log('info', 'API listening on port %d', config.bind.port)
+  extern.logger.log('info', 'API listening on port %d', config.bind.port);
 });
