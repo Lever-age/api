8e1709871f655c1825f16b6aede634fedd09f14e
James Lott <jlott@linode.com>
2016-09-30 15:25:28 -0400

    Implement endpoint for /campaigns/:id/info

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 
+/* External modules */
 
-var express = require('express'),
-  config = require('./config/config'),
-  glob = require('glob');
+var fs = require('fs')
+var express = require('express');
+var winston = require('winston');
+
+/* Library modules */
+
+var CampaignInfoStorage = require('./lib/storage/sqlite/campaign-info-storage');
+
+/* Controllers */
+
+var campaigninfo_by_id = require('./controllers/campaigns').campaigninfo_by_id;
+
+/* App variables */
 
 var app = express();
+var config = JSON.parse(fs.readFileSync('config.json'));
+var extern = new Object();
 
-require('./config/express')(app, config);
+extern.logger = winston;
 
-app.listen(config.port, function () {
-  console.log('Express server listening on port ' + config.port);
+/* Endpoints */
+
+app.get('/campaigns/:id/info', function (req, res) {
+  extern.backend = new CampaignInfoStorage(config.storage);
+  campaigninfo_by_id(extern, req, res);
 });
 
+/* Initialize */
+
+app.listen(config.bind.port, config.bind.address, function () {
+  extern.logger.log('info', 'API listening on port %d', config.bind.port)
+});
