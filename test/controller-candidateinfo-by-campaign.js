3d4b798c5a463669ae38949616d38b222d7af28c
James Lott <james@lottspot.com>
2016-10-01 11:46:49 -0400

    Better maintainability for controller tests

--- test/controller-candidateinfo-by-campaign.js
+++ test/controller-candidateinfo-by-campaign.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var expect           = require('chai').expect;
-var campaigninfoById = require('../controllers/campaigns').campaigninfoById;
+var expect                  = require('chai').expect;
+var candidateinfoByCampaign = require('../controllers/campaigns').candidateinfoByCampaign;
 
-describe('campaigninfoById', function(){
+describe('candidateinfoByCampaign', function(){
   var req = {
     params: {}
   };
   it('sends the retrieved object as a response', function(done){
     var backend = {
-      fetch_by_id: function(id, cb){ cb(undefined, 'response content'); }
+      fetch_by_campaign: function(id, cb){ cb(undefined, 'response content'); }
     };
     var res = {
       json: function(content){
@@ -17,16 +17,16 @@ describe('campaigninfoById', function(){
         done();
       }
     };
-    campaigninfoById({backend: backend}, req, res);
+    candidateinfoByCampaign({backend: backend}, req, res);
   });
   it('logs and sends error response', function(done){
     var backend = {
-      fetch_by_id: function(id, cb){ cb({name: 'test error', message: 'error content'}); }
+      fetch_by_campaign: function(id, cb){ cb({name: 'test error', message: 'error content'}); }
     };
     var logger = {
       log: function(level, format, name, message){
         expect(level, 'incorrect log level specified').to.equal('error');
-        expect(format, 'unexpected format string').to.equal('campaigninfoById: %s: %s');
+        expect(format, 'unexpected format string').to.equal('candidateinfoByCampaign: %s: %s');
         expect(name, 'unexpected error name').to.equal('test error');
         expect(message, 'unexpected error message').to.equal('error content');
       }
@@ -37,6 +37,6 @@ describe('campaigninfoById', function(){
         done();
       }
     };
-    campaigninfoById({backend: backend, logger: logger}, req, res);
+    candidateinfoByCampaign({backend: backend, logger: logger}, req, res);
   });
 });
