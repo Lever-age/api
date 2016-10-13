cbf280937c30f0c9c1c0d8198b578d908c434f62
James Lott <jlott@linode.com>
2016-10-13 19:29:11 -0400

    ES6-ify anonymous functions in tests

--- test/sqlite-candidate-info-storage.js
+++ test/sqlite-candidate-info-storage.js
@@ -3,13 +3,13 @@
 var expect              = require('chai').expect;
 var CandidateInfoStorage = require('../lib/storage/sqlite/candidate-info-storage');
 
-describe('CandidateInfoStorage', function(){
+describe('CandidateInfoStorage', () => {
 
   var storage = new CandidateInfoStorage({ path: 'test/data/db.sqlite' });
 
-  describe('#fetch_by_campaign', function(){
-    it('retrieves the expected record', function(done){
-      storage.fetch_by_campaign(1, function(err, cinfo){
+  describe('#fetch_by_campaign', () => {
+    it('retrieves the expected record', (done) => {
+      storage.fetch_by_campaign(1, (err, cinfo) => {
         if (err) return done(err);
         expect(cinfo.candidate_id, 'unexpected candidate_id').to.equal(1);
         expect(cinfo.candidate_name, 'unexpected candidate_name').to.equal('Matt Wolfe');
