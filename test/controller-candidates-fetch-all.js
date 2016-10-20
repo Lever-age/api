f93dd9869d66369ee86d69a5882ff33a300e83ef
Remmy <goremmy@gmail.com>
2016-10-20 19:48:49 -0400

    Remove dead code

--- test/controller-candidates-fetch-all.js
+++ test/controller-candidates-fetch-all.js
@@ -1,31 +1,23 @@
 'use strict';
 
-var fs = require('fs');
-// var winston = require('winston');
+// var fs = require('fs');
 var chai = require('chai');
 var expect = require('chai').expect;
 var chaiHttp = require('chai-http');
 var server = require('../app');
 // var config = JSON.parse(fs.readFileSync('config.test.json'));
-// var extern = { logger: winston };
-// var CandidateStorage = require('../lib/storage/sqlite/candidates');
-// var candidatesList = require('../controllers/candidates').candidatesList;
 
 chai.use(chaiHttp);
 
 describe('candidatesList', () => {
-  var req = {
-    params: {}
-  };
-
   it('should retreive all candidates on /candidates GET', () => {
     chai.request(server)
-    .get('/candidates/all')
+    .get('/candidates')
     .end((err, res) => {
-      res.should.have.status(200);
-      expect(res.body).to.be.an('object');
+      expect(res).have.have.status(200);
+      expect(res.body).to.have.length.of.at.least(3);
+      expect(res.body).to.be.an('array');
       expect(res.body).to.not.be.empty;
-      done();
     });
   });
 
