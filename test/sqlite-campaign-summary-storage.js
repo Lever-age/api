b47428c882b65cc5acb27836b5c80818e15f63c1
James Lott <james@lottspot.com>
2016-10-23 16:57:13 -0400

    Better naming for campaign summary storage methods

--- test/sqlite-campaign-summary-storage.js
+++ test/sqlite-campaign-summary-storage.js
@@ -7,9 +7,9 @@ describe('CampaignSummaryStorage', () => {
 
   var storage = new CampaignSummaryStorage({ path: 'test/data/db.sqlite' });
 
-  describe('#fetchByCampaign', () => {
+  describe('#fetchById', () => {
     it('retrieves the expected record', (done) => {
-      storage.fetchByCampaign(1, (err, summaries) => {
+      storage.fetchById(1, (err, summaries) => {
         if (err) return done(err);
         expect(summaries[0].campaign_id, 'unexpected campaign_id').to.equal(1);
         expect(summaries[0].summary_level, 'unexpected summary_level').to.equal('10');
